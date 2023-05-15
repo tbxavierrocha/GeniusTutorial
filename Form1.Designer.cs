227342647070bc82133a76150e941f79e85940c0
tbxavierrocha <tulio.fem@gmail.com>
2023-05-15 17:17:15 -0300

    first commit - não funciona

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbAzul = new System.Windows.Forms.PictureBox();
+            this.pbVermelho = new System.Windows.Forms.PictureBox();
+            this.pbAmarelo = new System.Windows.Forms.PictureBox();
+            this.pbVerde = new System.Windows.Forms.PictureBox();
+            this.lblPontos = new System.Windows.Forms.Label();
+            this.mstMenu = new System.Windows.Forms.MenuStrip();
+            this.mstArquivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAzul)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVermelho)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAmarelo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVerde)).BeginInit();
+            this.mstMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbAzul
+            // 
+            this.pbAzul.BackColor = System.Drawing.Color.Blue;
+            this.pbAzul.Enabled = false;
+            this.pbAzul.Location = new System.Drawing.Point(60, 60);
+            this.pbAzul.Name = "pbAzul";
+            this.pbAzul.Size = new System.Drawing.Size(200, 200);
+            this.pbAzul.TabIndex = 0;
+            this.pbAzul.TabStop = false;
+            this.pbAzul.Tag = "Azul";
+            this.pbAzul.Click += new System.EventHandler(this.CliqueReferencia_Clik);
+            // 
+            // pbVermelho
+            // 
+            this.pbVermelho.BackColor = System.Drawing.Color.DarkRed;
+            this.pbVermelho.Enabled = false;
+            this.pbVermelho.Location = new System.Drawing.Point(320, 60);
+            this.pbVermelho.Name = "pbVermelho";
+            this.pbVermelho.Size = new System.Drawing.Size(200, 200);
+            this.pbVermelho.TabIndex = 1;
+            this.pbVermelho.TabStop = false;
+            this.pbVermelho.Tag = "Vermelho";
+            this.pbVermelho.Click += new System.EventHandler(this.CliqueReferencia_Clik);
+            // 
+            // pbAmarelo
+            // 
+            this.pbAmarelo.BackColor = System.Drawing.Color.Goldenrod;
+            this.pbAmarelo.Enabled = false;
+            this.pbAmarelo.Location = new System.Drawing.Point(60, 315);
+            this.pbAmarelo.Name = "pbAmarelo";
+            this.pbAmarelo.Size = new System.Drawing.Size(200, 200);
+            this.pbAmarelo.TabIndex = 2;
+            this.pbAmarelo.TabStop = false;
+            this.pbAmarelo.Tag = "Amarelo";
+            this.pbAmarelo.Click += new System.EventHandler(this.CliqueReferencia_Clik);
+            // 
+            // pbVerde
+            // 
+            this.pbVerde.BackColor = System.Drawing.Color.DarkGreen;
+            this.pbVerde.Enabled = false;
+            this.pbVerde.Location = new System.Drawing.Point(320, 315);
+            this.pbVerde.Name = "pbVerde";
+            this.pbVerde.Size = new System.Drawing.Size(200, 200);
+            this.pbVerde.TabIndex = 3;
+            this.pbVerde.TabStop = false;
+            this.pbVerde.Click += new System.EventHandler(this.CliqueReferencia_Clik);
+            // 
+            // lblPontos
+            // 
+            this.lblPontos.AutoSize = true;
+            this.lblPontos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPontos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblPontos.Location = new System.Drawing.Point(66, 588);
+            this.lblPontos.Name = "lblPontos";
+            this.lblPontos.Size = new System.Drawing.Size(101, 29);
+            this.lblPontos.TabIndex = 4;
+            this.lblPontos.Text = "Pontos:";
+            // 
+            // mstMenu
+            // 
+            this.mstMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mstArquivo});
+            this.mstMenu.Location = new System.Drawing.Point(0, 0);
+            this.mstMenu.Name = "mstMenu";
+            this.mstMenu.Size = new System.Drawing.Size(584, 24);
+            this.mstMenu.TabIndex = 5;
+            this.mstMenu.Text = "menuStrip1";
+            // 
+            // mstArquivo
+            // 
+            this.mstArquivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.sobreToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.mstArquivo.Name = "mstArquivo";
+            this.mstArquivo.Size = new System.Drawing.Size(61, 20);
+            this.mstArquivo.Text = "Arquivo";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(584, 661);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblPontos);
+            this.Controls.Add(this.pbVerde);
+            this.Controls.Add(this.pbAmarelo);
+            this.Controls.Add(this.pbVermelho);
+            this.Controls.Add(this.pbAzul);
+            this.Controls.Add(this.mstMenu);
+            this.MainMenuStrip = this.mstMenu;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo Genius";
+            ((System.ComponentModel.ISupportInitialize)(this.pbAzul)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVermelho)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAmarelo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbVerde)).EndInit();
+            this.mstMenu.ResumeLayout(false);
+            this.mstMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbAzul;
+        private System.Windows.Forms.PictureBox pbVermelho;
+        private System.Windows.Forms.PictureBox pbAmarelo;
+        private System.Windows.Forms.PictureBox pbVerde;
+        private System.Windows.Forms.Label lblPontos;
+        private System.Windows.Forms.MenuStrip mstMenu;
+        private System.Windows.Forms.ToolStripMenuItem mstArquivo;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,154 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Media;
 using System.Windows.Forms;
 
 namespace GeniusTutorial
 {
     public partial class Form1 : Form
     {
+        List<string> SequenciaCores = new List<string>();
+        List<string> SequenciaJogador = new List<string>();
+        string[] cores = { "Vermelho", "Amarelo", "Azul", "Verde" };
+        string atualJogador, atualCor;
+        int pontos, indexLista;
+        bool podeJogar;
+        Random rdn = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SortearCores()
+        {
+            atualCor = cores[rdn.Next(0, cores.Length)];
+            SequenciaCores.Add(atualCor);
+
+            foreach (var cor in SequenciaCores)
+            {
+                Application.DoEvents();
+                Thread.Sleep(250);
+                MostrarCor(cor);
+            }
+            PodeJogar();
+        }
+
+        private void MostrarCor(string atualCor)
+        {
+            switch (atualCor)
+            {
+                case "Vermelho":
+                    pbVermelho.BackColor = Color.Red;
+                    SystemSounds.Exclamation.Play();
+                    Application.DoEvents();
+                    Thread.Sleep(800);
+                    pbVermelho.BackColor = Color.DarkRed;
+                    break;
+
+                case "Amarelo":
+                    pbAmarelo.BackColor = Color.Yellow;
+                    SystemSounds.Beep.Play();
+                    Application.DoEvents();
+                    Thread.Sleep(800);
+                    pbAmarelo.BackColor = Color.Goldenrod;
+                    break;
+
+                case "Azul":
+                    pbAzul.BackColor = Color.Cyan;
+                    SystemSounds.Asterisk.Play();
+                    Application.DoEvents();
+                    Thread.Sleep(800);
+                    pbAzul.BackColor = Color.Blue;
+                    break;
+
+                case "Verde":
+                    pbVerde.BackColor = Color.Lime;
+                    SystemSounds.Hand.Play();
+                    Application.DoEvents();
+                    Thread.Sleep(800);
+                    pbVerde.BackColor = Color.DarkGreen;
+                    break;
+
+
+            }
+        }
+
+        private void PodeJogar()
+        {
+            foreach (PictureBox pb in Controls.OfType<PictureBox>())
+            {
+                pb.Enabled = true;
+            }
+
+            podeJogar = true;
+        }
+
+        private void CliqueReferencia_Clik(object sender, EventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (podeJogar)
+            {
+                atualJogador = pic.Tag.ToString();
+                podeJogar = false;
+                SequenciaJogador.Add(atualJogador);
+                MostrarCor(atualJogador);
+
+                if (SequenciaJogador[indexLista] == SequenciaCores[indexLista])
+                {
+                    indexLista++; pontos++; lblPontos.Text = "Pontos: " + pontos.ToString();
+                    ChecarFinal();
+                }
+                else
+                {
+                    MessageBox.Show("Sequência incorreta! Fim de jogo.");
+                }
+            }
+        }
+
+        private void ChecarFinal()
+        {
+            if (indexLista == SequenciaCores.Count)
+            {
+                Application.DoEvents();
+                Thread.Sleep(450);
+                indexLista = 0;
+                SequenciaJogador.Clear();
+                SortearCores();
+            }
+            else
+            {
+                podeJogar = true;
+            }
+        }
+
+        private void MenuReferencia_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menu = (ToolStripMenuItem)sender;
+
+            switch (menu.Tag.ToString())
+            {
+                case "Inicio":
+                    pontos = 0; lblPontos.Text = "Pontos: " + pontos.ToString();
+                    indexLista = 0; podeJogar = false;
+                    SequenciaCores.Clear(); SequenciaJogador.Clear();
+                    SortearCores();
+                    break;
+
+                case "Sobre":
+                    if (podeJogar)
+                    {
+                        string infos = "Nome do jogo: Genius \n Copiado por: Tulio";
+                        MessageBox.Show(infos, "Sobre o jogo");
+                    }
+                    break;
+
+                case "Sair":
+                    DialogResult msg = MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo);
+                    if (msg == DialogResult.Yes) { Application.Exit(); }
+                    break;
+            }
         }
     }
 }
